bb40f7811bb635b3c3bd93a044af39fa3693117f
Md.Jewel mia <md.jewelmia52@gmail.com>
2023-12-12 07:12:37 +0600

    Initial commit and graphql setup

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+    }),
+  ],
+})
+export class AppModule {}
